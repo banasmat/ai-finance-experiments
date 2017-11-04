aebcbb3291a371a54a1e1c767e3c47acea8f3418
Mateusz Banaś <banasmat@gmail.com>
2017-11-04 23:26:26 +0100

    Added empty DataVisualizer
    Former-commit-id: 32c44f6b4adefae1154d92dff978aa25d3c67dca
    Former-commit-id: a4dc5111eb9ad8eac6153145a14378b92191020a

---
+++ DataVisualizer.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib as plt
+from datetime import datetime, timedelta
+
+
+class DataVisualizer(object):
+
+    def plot(self, x_train, y_train, x_test, y_test):
+
+        return
--- ForexAnalyzer.py
+++ ForexAnalyzer.py
@@ -1,9 +1,11 @@
-import numpy as np
-
 from DataProvider import DataProvider
+from DataVisualizer import DataVisualizer
 
 data_provider = DataProvider()
+data_visualizer = DataVisualizer()
 
-x_train, y_train, x_test, y_test = data_provider.get_data()
+x_train, y_train, x_test, y_test = data_provider.get_data(False)
+
+# TODO data_visualizer will need price data + labels(y)
 
 print(x_train, y_train, x_test, y_test)
