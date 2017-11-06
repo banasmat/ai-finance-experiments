8dd47657b2fd78f0327bf9fbe629524196efe82c
Mateusz Banaś <banasmat@gmail.com>
2017-11-06 23:47:10 +0100

    Added debugging notes when preparing labels
    Former-commit-id: 586c3dadea4dfb4524898f4cbdfb2e2103ab2625
    Former-commit-id: a4d83db9c451d604933b684f3d37e9403bdb94d0

--- DataSetProvider.py
+++ DataSetProvider.py
@@ -28,46 +28,55 @@ class DataSetProvider(object):
 
         labels = []
 
-        for index, n in news.iterrows():
+        with open('output/notes.txt', 'a') as notes_file:
+            for index, n in news.iterrows():
 
-            #TODO might check also larger intervals
-            datetime_plus_interval = n['datetime'] + timedelta(hours=12)
+                #TODO might check also larger intervals
+                datetime_plus_interval = n['datetime'] + timedelta(hours=12)
 
-            i = 0
+                i = 0
 
-            while True:
-                prices_affected_by_news = prices.loc[(prices.index >= n['datetime']) & (prices.index <= datetime_plus_interval)]
+                while True:
+                    prices_affected_by_news = prices.loc[(prices.index >= n['datetime']) & (prices.index <= datetime_plus_interval)]
+                    if i > 10:
+                        break
+                    elif len(prices_affected_by_news) < 100:
+                        datetime_plus_interval += timedelta(hours=12)
+                        i += 1
+                    else:
+                        break
+
                 if i > 10:
-                    break
-                elif len(prices_affected_by_news) < 100:
-                    datetime_plus_interval += timedelta(hours=12)
-                    i += 1
-                else:
+                    print('last date: ', n['datetime'])
                     break
 
-            if i > 10:
-                print('last date: ', n['datetime'])
-                break
+                price_when_news_happens = prices_affected_by_news.loc[prices_affected_by_news.index[0]]['mean']
 
-            price_when_news_happens = prices_affected_by_news.loc[prices_affected_by_news.index[0]]['mean']
+                # TODO mean might not be the best approach. Try also e.g. highest value
+                price_mean_in_affected_period = prices_affected_by_news['mean'].mean()
 
-            # TODO mean might not be the best approach. Try also e.g. highest value
-            price_mean_in_affected_period = prices_affected_by_news['mean'].mean()
+                diff = price_mean_in_affected_period - price_when_news_happens
 
-            diff = price_mean_in_affected_period - price_when_news_happens
+                diff_percent = abs((diff / price_when_news_happens) * 100)
 
-            diff_percent = abs(diff / price_when_news_happens * 100)
+                diff_threshold = 0.5
+                label = 0
+                if diff_percent > diff_threshold:
+                    if diff > 0:
+                        label = 1
+                    else:
+                        label = -1
 
-            # TODO check percent ?
-            diff_threshold = 0.5
-            label = 0
-            if abs(diff_percent) > diff_threshold:
-                if diff > 0:
-                    label = 1
-                else:
-                    label = -1
+                notes_file.write('news datetime: ' + n['datetime'].strftime('%Y-%m-%d %H%M') + "\n")
+                notes_file.write('price when news happens: ' + str(price_when_news_happens) + "\n")
+                notes_file.write('label: ' + str(label) + "\n")
+                notes_file.write('datetime plus interval: ' + datetime_plus_interval.strftime('%Y-%m-%d %H%M') + "\n")
+                notes_file.write('price mean in affected period: ' + str(price_mean_in_affected_period) + "\n")
+                notes_file.write('diff: ' + str(diff) + "\n")
+                notes_file.write('diff percent: ' + str(diff_percent) + "\n")
+                notes_file.write("\n")
 
-            labels.append(label)
+                labels.append(label)
 
         #TODO parametrize interval
 
--- ForexAnalyzer.py
+++ ForexAnalyzer.py
@@ -5,6 +5,6 @@ import numpy as np
 data_set_provider = DataSetProvider()
 data_visualizer = DataVisualizer()
 
-x_train, y_train, x_test, y_test = data_set_provider.get_data(False)
+x_train, y_train, x_test, y_test = data_set_provider.get_data(True)
 
 data_visualizer.visualize(np.append(y_train, y_test))
