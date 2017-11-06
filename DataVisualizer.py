2c4272fc656c09e70e1978016739eb1f6bdddd54
Mateusz Banaś <banasmat@gmail.com>
2017-11-06 15:29:24 +0100

    Displaying simple graph with prices and news
    Former-commit-id: f0821fd4bdaaf236ad6db68b054bf7d0f11d19f8
    Former-commit-id: 79da1a0ac02c977e7b1255fedea1cf04d02a1410

--- DataVisualizer.py
+++ DataVisualizer.py
@@ -11,18 +11,44 @@ class DataVisualizer(object):
 
     def visualize(self, labels):
 
+        plt.style.use('ggplot')
+
         prices = self.prep_data_provider.get_price_data()
         news = self.prep_data_provider.get_news_data(prices.index[0])
 
-        x = news.as_matrix()[:len(labels)]
-        y = np.array(labels)
 
-        print('labels: ', len(labels))
-        print('news: ', len(news))
 
-        # TODO insert labels on price plot
+        news = news.iloc[:len(labels)]
+        labels = pd.DataFrame(list(map(lambda label: label is not 0, labels)))
+        labels.index = news['datetime']
 
-        # plt.plot(prices)
-        # plt.show()
+        prices = prices['mean'].resample('1H').mean()
+       # labels = labels[0].resample('1H').mean()
+
+        price_xs = prices.index.tolist()
+        price_ys = prices.values.tolist()
+
+        price_xs = list(map(self.__timestamp_to_datetime_string, price_xs))
+
+        news_xs = []
+        news_ys = []
+
+        print('start processing news')
+
+        for label_datetime, label in labels.iterrows():
+            news_xs.append(self.__timestamp_to_datetime_string(label_datetime))
+            news_ys.append(prices.loc[label_datetime.round('h')])
+
+        print('start plotting prices')
+        plt.plot(price_xs[:1000], price_ys[:1000], color='blue')
+        print('start plotting news')
+        plt.scatter(news_xs[:100], news_ys[:100], color='green')
+
+        # TODO scroll graph
+
+        plt.show()
 
         return
+
+    def __timestamp_to_datetime_string(self, timestamp):
+        return timestamp.strftime('%Y-%m-%d %H')
