799cea306a4d6903ee4a233b88e6626bd831b624
Mateusz Banaś <banasmat@gmail.com>
2017-11-18 20:18:01 +0100

    Preparation for floydhub training
    Former-commit-id: 4c7c5780d573f07459a313eeae94de08fa4352bd
    Former-commit-id: 6fd0cf91abd79c110a9863fc3721669a741de40f

--- floydhub/KerasNeuralNetwork.py
+++ floydhub/KerasNeuralNetwork.py
@@ -8,23 +8,23 @@ class KerasNeuralNetwork:
 
     @staticmethod
     def train(x_train: np.ndarray, y_train: np.ndarray, x_test: np.ndarray, y_test: np.ndarray):
-        model = Sequential()
 
         batch_size = 32
-        epochs = 5
-
-        print('x_train', x_train.shape)
-        print('y_train', y_train.shape)
-        print('x_test', x_test.shape)
-        print('y_test', y_test.shape)
+        epochs = 100
 
         x_input_len = x_train.shape[1]
         y_input_len = y_train.shape[1]
 
         model = Sequential()
+
         model.add(Dense(512, input_shape=(x_input_len,)))
         model.add(Activation('relu'))
         model.add(Dropout(0.5))
+
+        model.add(Dense(512, input_shape=(x_input_len,)))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.5))
+
         model.add(Dense(y_input_len))
         model.add(Activation('softmax'))
 
@@ -40,10 +40,10 @@ class KerasNeuralNetwork:
 
         print(model.summary())
 
-        model.fit(x_train, y_train, epochs=5, batch_size=64)
+        # model.fit(x_train, y_train, epochs=5, batch_size=64)
 
         loss_and_metrics = model.evaluate(x_test, y_test, batch_size=128)
 
         model.save('forex_analyzer_model.h5')
 
-        print(loss_and_metrics)
+        print('accuracy', loss_and_metrics[1])
--- floydhub/main.py
+++ floydhub/main.py
@@ -1,15 +1,12 @@
 from KerasNeuralNetwork import KerasNeuralNetwork
 import pickle
-from keras.datasets import fashion_mnist
 import numpy as np
 
-(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
-
-# with open('/dataset/data.pickle', 'rb') as f:
-with open('../output/data.pickle', 'rb') as f:
+with open('/dataset/data.pickle', 'rb') as f:
+# with open('../output/data.pickle', 'rb') as f:
     x_train, y_train, x_test, y_test = pickle.load(f)
 
-x_train = np.nan_to_num(x_train.round(4))
-x_test = np.nan_to_num(x_test.round(4))
+x_train = np.nan_to_num(x_train.round(8))
+x_test = np.nan_to_num(x_test.round(8))
 
 KerasNeuralNetwork.train(x_train, y_train.astype(float), x_test.round(0), y_test.astype(float))
