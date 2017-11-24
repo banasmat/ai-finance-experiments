Fixed missing forecast and previous values in dataset. Scaling actual value larger than other
Former-commit-id: 44590d852810063ea0f26c0cf22dea609b28fb1f
Former-commit-id: 23298a86b8b9645464d5f0f07cea408f3dca57cf

DataSetProvider.py
@@ -27,7 +27,7 @@ class DataSetProvider(object):
 
         price_news_map = {}
 
-        # symbol_pairs = symbol_pairs[3:5]
+        symbol_pairs = symbol_pairs[3:4]
 
         for symbol_pair in symbol_pairs:
 
@@ -53,9 +53,16 @@ class DataSetProvider(object):
             news = self.feature_provider.add_preceding_price_feature(prices, news, symbol_pair[0] + symbol_pair[1],
                                                                 refresh=True)
 
+            # TODO features:
+            # - rolling mean instead of price mean(?)
+            # - volume (we don't have the data)
+            # - rolling mean in last 24, 12, 6, now ?
+            # OR recurrent neural network
+            # + news as additional feature
+
             labels = self.labels_provider.get_labels(prices, news, symbol_pair[0] + symbol_pair[1], refresh=True)
 
-            # data_visualizer.visualize(prices, news, labels)
+            # self.data_visualizer.visualize(prices, news, labels)
 
             x_train, y_train, x_test, y_test = self.__get_data_set(news, labels,
                                                                    self.prep_data_provider.get_all_titles(),
@@ -80,6 +87,7 @@ class DataSetProvider(object):
                 y_test_all = np.append(y_test_all, y_test, axis=0)
 
             print(len(x_train_all))
+        return x_train_all, y_train_all, x_test_all, y_test_all
 
     def __get_data_set(self, news: pd.DataFrame, labels: np.ndarray, all_titles: List, all_currencies: List, all_pairs: List) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
 

ForexAnalyzer.py
@@ -1,29 +1,35 @@
 from DataSetProvider import DataSetProvider
-from NeuralNetwork import NeuralNetwork
+# from NeuralNetwork import NeuralNetwork
 import numpy as np
 import pickle
 from pathlib import Path
 
 data_set_provider = DataSetProvider()
-neural_network = NeuralNetwork()
+# neural_network = NeuralNetwork()
 
 
-if Path('output/x_train_all.npy').is_file():
-    x_train_all = np.load('output/x_train_all.npy')
-    y_train_all = np.load('output/y_train_all.npy')
-    x_test_all = np.load('output/x_test_all.npy')
-    y_test_all = np.load('output/y_test_all.npy')
-else:
-    x_train_all, y_train_all, x_test_all, y_test_all = data_set_provider.prepare_data_set()
+# if Path('output/x_train_all.npy').is_file():
+#     x_train_all = np.load('output/x_train_all.npy')
+#     y_train_all = np.load('output/y_train_all.npy')
+#     x_test_all = np.load('output/x_test_all.npy')
+#     y_test_all = np.load('output/y_test_all.npy')
+# else:
+#     x_train_all, y_train_all, x_test_all, y_test_all = data_set_provider.prepare_data_set()
+#
+#     np.save('output/x_train_all.npy', x_train_all)
+#     np.save('output/y_train_all.npy', y_train_all)
+#     np.save('output/x_test_all.npy', x_test_all)
+#     np.save('output/y_test_all.npy', y_test_all)
+#
+# #TODO testuj na [3:5]
+# # wstaw jakieś indexy do newsów
+# # LUB zmień sieć na rekurencyjną, a newsy będą dodatkowymi featurami
+# #
+#
+# data = x_train_all, y_train_all, x_test_all, y_test_all
+#
+# with open('output/data.pickle', 'wb') as f:
+#     pickle.dump(data, f)
 
-    np.save('output/x_train_all.npy', x_train_all)
-    np.save('output/y_train_all.npy', y_train_all)
-    np.save('output/x_test_all.npy', x_test_all)
-    np.save('output/y_test_all.npy', y_test_all)
-
-data = x_train_all, y_train_all, x_test_all, y_test_all
-
-with open('output/data.pickle', 'wb') as f:
-    pickle.dump(data, f)
-
+x_train_all, y_train_all, x_test_all, y_test_all = data_set_provider.prepare_data_set()
 # neural_network.train(x_train_all, y_train_all, x_test_all, y_test_all)