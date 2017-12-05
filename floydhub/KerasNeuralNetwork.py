4d82dd7a1c9fbd2305a90dfa8ab9eee5d406a261
Mateusz Banaś <banasmat@gmail.com>
2017-12-05 17:53:51 +0100

    Using KerasRegressor
    Former-commit-id: 50c72ae446e357995cc9da78c8a4b2b2589e6488
    Former-commit-id: 2461e2cb41529f584ceb0894a3664469feb5c3a9

--- floydhub/KerasNeuralNetwork.py
+++ floydhub/KerasNeuralNetwork.py
@@ -1,56 +1,88 @@
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense, LSTM, TimeDistributed, Dropout, Activation
-
+from keras.layers import Dense
+from sklearn.model_selection import KFold, cross_val_score
+from keras.wrappers.scikit_learn import KerasRegressor
+from sklearn.metrics import accuracy_score
 
 class KerasNeuralNetwork:
 
-    @staticmethod
-    def train(x_train: np.ndarray, y_train: np.ndarray, x_test: np.ndarray, y_test: np.ndarray):
+    x_input_len = 0
+    y_input_len = 1
 
-        x_input_len = x_train.shape[1]
-        y_input_len = 1
 
-        x_y_input_len_avg = int((x_input_len + y_input_len)/2)
+    def train(self, x_train: np.ndarray, y_train: np.ndarray, x_test: np.ndarray, y_test: np.ndarray):
+
+        self.x_input_len = x_train.shape[1]
+
+        model = KerasRegressor(build_fn=self.build_model, batch_size=10, nb_epoch=10)
+        kfold = KFold(n_splits=10, random_state=1)
+        results = cross_val_score(estimator=model, X=x_train, y=y_train, cv=kfold, n_jobs=-1)
+
+        print("Results: %.2f (%.2f) MSE" % (results.mean(), results.std()))
+
+        # model = self.build_model()
+        # model.fit(x_train, y_train,
+        #           batch_size=10,
+        #           epochs=10,
+        #           # verbose=1,
+        #           # validation_split=0.1
+        #           )
+
+        # parameters = {'batch_size': [32],
+        #               'epochs': [1],
+        #               'optimizer': ['adam'],
+        #               'loss': ['mean_squared_error']}
+        # grid_search = GridSearchCV(estimator=model,
+        #                            param_grid=parameters,
+        #                            scoring='accuracy',
+        #                            cv=10,
+        #                            n_jobs=-1)
+        # grid_search = grid_search.fit(x_train, y_train)
+        # print(grid_search)
+
+        # best_parameters = grid_search.best_params_
+        # best_accuracy = grid_search.best_score_
+
+        # print('model summary', model.summary())
+
+        # print('best parameters', best_parameters)
+        # print('best accuracy', best_accuracy)
+
+        # loss_and_metrics = model.evaluate(x_test, y_test, batch_size=128)
+        #
+        # model.save('forex_analyzer_model.h5')
+
+        # print('')
+        # print('accuracy', loss_and_metrics[1])
+
+    def build_model(self, optimizer='adam', loss='mean_squared_error'):
+
+        x_y_input_len_avg = int((self.x_input_len + self.y_input_len) / 2)
 
         model = Sequential()
 
         model.add(Dense(units=x_y_input_len_avg,
                         activation='relu',
                         kernel_initializer='uniform',
-                        input_shape=(x_input_len,)
+                        input_shape=(self.x_input_len,)
                         ))
-        #TODO activate dropout in case of overfitting (big difference between training and testing accuracy)
-        #model.add(Dropout(p=0.1))
+        # TODO activate dropout in case of overfitting (big difference between training and testing accuracy)
+        # model.add(Dropout(p=0.1))
 
         model.add(Dense(units=x_y_input_len_avg,
                         activation='relu',
                         kernel_initializer='uniform',
                         ))
-        #model.add(Dropout(p=0.1))
+        # model.add(Dropout(p=0.1))
 
-        model.add(Dense(units=y_input_len,
+        model.add(Dense(units=self.y_input_len,
                         activation='tanh',
                         kernel_initializer='uniform',
                         ))
 
-        model.compile(loss='mean_squared_error',
-                      optimizer='adam',
+        model.compile(loss=loss,
+                      optimizer=optimizer,
                       metrics=['accuracy'])
 
-        model.fit(x_train, y_train,
-                  batch_size=10,
-                  epochs=100,
-                  # verbose=1,
-                  # validation_split=0.1
-                  )
-
-        print(model.summary())
-
-        # model.fit(x_train, y_train, epochs=5, batch_size=64)
-
-        loss_and_metrics = model.evaluate(x_test, y_test, batch_size=128)
-
-        model.save('forex_analyzer_model.h5')
-
-        print('accuracy', loss_and_metrics[1])
+        return model
--- floydhub/main.py
+++ floydhub/main.py
@@ -2,11 +2,14 @@ from KerasNeuralNetwork import KerasNeuralNetwork
 import pickle
 import numpy as np
 
-with open('/dataset/data.pickle', 'rb') as f:
-# with open('../output/floydhub/data.pickle', 'rb') as f:
+# with open('/dataset/data.pickle', 'rb') as f:
+with open('../output/floydhub/data.pickle', 'rb') as f:
     x_train, y_train, x_test, y_test = pickle.load(f)
 
 x_train = np.nan_to_num(x_train.round(8))
 x_test = np.nan_to_num(x_test.round(8))
 
-KerasNeuralNetwork.train(x_train, y_train.astype(float), x_test.round(0), y_test.astype(float))
+nn = KerasNeuralNetwork()
+
+if __name__ == "__main__":
+    nn.train(x_train, y_train.astype(float), x_test.round(0), y_test.astype(float))
