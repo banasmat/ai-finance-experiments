287d910874047884fb7c40edf4e45ee0edc97ba2
Mateusz Banaś <banasmat@gmail.com>
2017-12-10 23:43:06 +0100

    Fetching live price records from api
    Former-commit-id: f24cc6985d0380d45eee3d69159a9fc087d6bd92
    Former-commit-id: 1fe5df5982b3df913d18fd169ba76bc56cd6b0ee

--- app/live_update/LivePriceFetcher.py
+++ app/live_update/LivePriceFetcher.py
@@ -1,20 +1,34 @@
 from urllib.request import urlopen, Request
 import xmltodict
-import sqlalchemy
+from collections import OrderedDict
+from app.model.PriceQuote import PriceQuote
+from app.database.Connection import Connection
+import datetime
 
 
 class LivePriceFetcher:
-    fetch_url = "http://rates.fxcm.com/RatesXML"
 
     def fetch(self):
-        req = Request(self.fetch_url, headers={'User-Agent': "Magic Browser"})
+
+        session = Connection.get_instance().get_session()
+
+        req = Request("http://rates.fxcm.com/RatesXML", headers={'User-Agent': "Magic Browser"})
         file = urlopen(req)
         data = file.read()
         file.close()
 
         data = xmltodict.parse(data)
-        print(data)
+        rates: OrderedDict = data.get('Rates').get('Rate')
 
+        for rate in rates:
+            symbol = rate.get('@Symbol')
+            #TODO get from saved list of trained symbols
+            if len(symbol) == 6:
+                dt = datetime.datetime.now()
+                time = datetime.datetime.strptime(rate.get('Last'), '%H:%M:%S')
+                dt = dt.replace(hour=time.hour, minute=time.minute, second=time.second)
+                quote = PriceQuote(symbol, dt, rate.get('High'), rate.get('Low'))
 
-fetcher = LivePriceFetcher()
-fetcher.fetch()
+                session.add(quote)
+
+        session.commit()
