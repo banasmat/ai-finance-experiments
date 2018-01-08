f47a4f6f2a65a2da182d0c457ab4aa5ceafcd252
Mateusz Banaś <banasmat@gmail.com>
2018-01-08 09:16:59 +0100

    Uncommented && git status
    Former-commit-id: c2944fe11513f9999b4e004ebebd68072ed8c493
    Former-commit-id: cc9c33bed897e73b31c45b0119da558a65849eb1

--- forex_analyzer.py
+++ forex_analyzer.py
@@ -8,12 +8,7 @@ from app.dataset.DataSetProvider import DataSetProvider
 data_set_provider = DataSetProvider()
 # neural_network = NeuralNetwork()
 
-#TODO
-# we have to prepare system for creating 1 element datasets for predictions
-# we have to save all symbols, symbol pairs, news titles, etc.
-# (what if new title appears?) ideally: save for later retraining
-# we can scrape news from website to csv
-# but how do we get fresh prices?
+#TODO nn should be retrained every month including new data
 
 # if Path('output/x_train_all.npy').is_file():
 
@@ -25,14 +20,14 @@ data_set_provider = DataSetProvider()
 if __name__ == '__main__':
     x_train_all, y_train_all, x_test_all, y_test_all = data_set_provider.prepare_full_data_set()
     print(x_train_all)
-# np.save('output/x_train_all.npy', x_train_all)
-# np.save('output/y_train_all.npy', y_train_all)
-# np.save('output/x_test_all.npy', x_test_all)
-# np.save('output/y_test_all.npy', y_test_all)
-#
-# data = x_train_all, y_train_all, x_test_all, y_test_all
-#
-# with open('output/floydhub/data.pickle', 'wb') as f:
-#     pickle.dump(data, f)
+    np.save('output/x_train_all.npy', x_train_all)
+    np.save('output/y_train_all.npy', y_train_all)
+    np.save('output/x_test_all.npy', x_test_all)
+    np.save('output/y_test_all.npy', y_test_all)
+
+    data = x_train_all, y_train_all, x_test_all, y_test_all
+
+    with open('output/floydhub/data.pickle', 'wb') as f:
+        pickle.dump(data, f)
 
 # neural_network.train(x_train_all, y_train_all, x_test_all, y_test_all)
