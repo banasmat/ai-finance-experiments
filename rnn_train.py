7cb19c04cfd0793f7f34121dc8b0054033dc6825
Mateusz Banaś <banasmat@gmail.com>
2018-01-19 10:09:37 +0100

    Script && git status
    Former-commit-id: dc8682156e007915174b5152fd8ccd1ec2f364d7
    Former-commit-id: 3fe2c4c967f63573fff47180e92a4c65a4f86ac6

--- rnn_train.py
+++ rnn_train.py
@@ -13,19 +13,14 @@ prices = prep_data_provider.get_price_records('EUR', 'USD', [0, 2, 3, 4, 5, 6])
 date_from = datetime.datetime.strptime('2005-01-01 01:00:00', '%Y-%m-%d %H:%M:%S')
 date_to = datetime.datetime.strptime('2017-12-01 00:00:00', '%Y-%m-%d %H:%M:%S')
 
-print(prices.tail())
 
 train_prices = prices.loc[(prices.index > date_from) & (prices.index < date_to)]
-
-print(train_prices.tail())
-
 x_train, y_train = dataset_provider.prepare_dataset(train_prices)
+
+nn.train(x_train, y_train)
 
 # test_prices = prices.loc[prices.index > date_until]
 # x_test, y_test = dataset_provider.prepare_dataset(test_prices)
 
-print(x_train)
-quit()
 
-nn.train(x_train, y_train)
 
