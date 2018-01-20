8586fc7f3d5244516b9bd53197ce23d7466ab062
Mateusz Banaś <banasmat@gmail.com>
2018-01-20 20:14:49 +0100

    Displaying && git status
    Former-commit-id: c86f88c64ed5378bdd2d2a40dd2c35624a13a687
    Former-commit-id: d5fafe88e43cbf0b6346a87c01dd05c039a69582

--- rnn_train.py
+++ rnn_train.py
@@ -15,22 +15,21 @@ prices = prep_data_provider.get_price_records('EUR', 'USD', [0, 5, 6])
 date_from = datetime.datetime.strptime('2005-01-01 01:00:00', '%Y-%m-%d %H:%M:%S')
 date_to = datetime.datetime.strptime('2017-12-01 00:00:00', '%Y-%m-%d %H:%M:%S')
 
+lstm_length = 120
 
 train_prices = prices.loc[(prices.index > date_from) & (prices.index < date_to)]
-x_train, y_train = dataset_provider.prepare_dataset(train_prices)
+x_train, y_train = dataset_provider.prepare_dataset(train_prices, lstm_length=lstm_length)
 
 # nn.train(x_train, y_train)
 
-test_prices = prices.loc[prices.index > date_to]
-x_test, y_test = dataset_provider.prepare_dataset(test_prices)
+test_prices = prices.loc[prices.index > (date_to - datetime.timedelta(hours=lstm_length))]
+x_test, y_test = dataset_provider.prepare_dataset(test_prices, lstm_length=lstm_length)
 
 scaled_predictions = nn.predict(x_test)
 predictions = dataset_provider.unscale_predictions(scaled_predictions)
+real_prices = dataset_provider.unscale_predictions(y_test)
 
-# print(test_prices.tail())
-# print(predictions)
-
-plt.plot(y_test, color='red', label='Real EURUSD Price')
+plt.plot(real_prices, color='red', label='Real EURUSD Price')
 plt.plot(predictions, color='blue', label='Predicted EURUSD Price')
 plt.title('EURUSD Price Prediction')
 plt.xlabel('Time')
