7e868b912609ac01ebe949c959ccb7fc267f3bd2
Mateusz Banaś <banasmat@gmail.com>
2018-03-13 21:51:56 +0100

    Gitignores update, prepared xbrl organizer to save every quarter to csv
    Former-commit-id: 94a8696a118f73e65a9c3ac9520ff3032064db17
    Former-commit-id: d9b7f3861baef79d6b4186821d523d281cc88a93

--- app/dataset/XBRLDataSetProvider.py
+++ app/dataset/XBRLDataSetProvider.py
@@ -78,27 +78,27 @@ class XBRLDataSetProvider(object):
         res_dir = os.path.join(os.path.abspath(os.getcwd()), 'scrapy', 'xbrl_output')
         output_dir = os.path.join(os.path.abspath(os.getcwd()), 'output', 'xbrl_organized')
 
-        all_quarters = os.listdir(res_dir)
-        all_quarters.remove('.gitignore')
-
-        all_ciks = []
-
-        for quarter_dir in os.listdir(res_dir):
-
-            quarter_dir = os.path.join(res_dir, quarter_dir)
-
-            if not os.path.isdir(quarter_dir):
-                continue
-
-            sub_file = os.path.join(quarter_dir, 'sub.txt')
-            subs = pd.read_csv(sub_file, sep='\t', encoding='ISO-8859-1', usecols=['cik'])
-            all_ciks = all_ciks + subs['cik'].tolist()
-
-        all_ciks = set(all_ciks)
+        # all_quarters = os.listdir(res_dir)
+        # all_quarters.remove('.gitignore')
+        #
+        # all_ciks = []
+        #
+        # for quarter_dir in os.listdir(res_dir):
+        #
+        #     quarter_dir = os.path.join(res_dir, quarter_dir)
+        #
+        #     if not os.path.isdir(quarter_dir):
+        #         continue
+        #
+        #     sub_file = os.path.join(quarter_dir, 'sub.txt')
+        #     subs = pd.read_csv(sub_file, sep='\t', encoding='ISO-8859-1', usecols=['cik'])
+        #     all_ciks = all_ciks + subs['cik'].tolist()
+        #
+        # all_ciks = set(all_ciks)
 
         # all_data = np.empty((len(all_quarters), ))
-        index = pd.MultiIndex.from_product([all_quarters, all_ciks], names=['quarter', 'cik'])
-        all_data = pd.DataFrame(index=index, columns=xbrl_titles.titles.keys())
+        # index = pd.MultiIndex.from_product([all_quarters, all_ciks], names=['quarter', 'cik'])
+        # all_data = pd.DataFrame(index=index, columns=xbrl_titles.titles.keys())
         pd.options.mode.chained_assignment = None
 
         for quarter_dir in os.listdir(res_dir):
@@ -117,8 +117,8 @@ class XBRLDataSetProvider(object):
             numbers = pd.read_csv(num_file, sep='\t', encoding='ISO-8859-1', usecols=['adsh', 'tag', 'value'])
             subs = pd.read_csv(sub_file, sep='\t', encoding='ISO-8859-1', usecols=['adsh', 'cik', 'name'])
             #
-            # all_data[quarter_dir] = pd.DataFrame(index=subs['cik'], columns=xbrl_titles.titles.keys())
-            # all_data[quarter_dir].fillna(0, inplace=True)
+            df = pd.DataFrame(index=subs['cik'], columns=xbrl_titles.titles.keys())
+            df.fillna(0, inplace=True)
             numbers = numbers.merge(subs, on='adsh', how='left')
 
             # i = 0
@@ -138,13 +138,19 @@ class XBRLDataSetProvider(object):
                         except KeyError:
                             continue
 
-                    all_data.loc[(all_data.index.get_level_values('quarter') == quarter_name)
-                                 & (all_data.index.get_level_values('cik') == cik), fixed_tag] = val
+                    if not np.isnan(val):
+                        df.loc[df.index == cik][fixed_tag] = val
+                    #
+                    # all_data.loc[(all_data.index.get_level_values('quarter') == quarter_name)
+                    #              & (all_data.index.get_level_values('cik') == cik), fixed_tag] = val
 
                 # break
+
+            with open(os.path.join(output_dir, quarter_name + '.csv'), 'w') as f:
+                df.to_csv(f)
             # break
 
-        all_data.fillna(0, inplace=True)
+        # all_data.fillna(0, inplace=True)
 
-        tensor = all_data.values.reshape((len(all_quarters), len(all_ciks), all_data.shape[1]))
-        np.save(os.path.join(output_dir, 'xbrl_data'), tensor)
+        # tensor = all_data.values.reshape((len(all_quarters), len(all_ciks), all_data.shape[1]))
+        # np.save(os.path.join(output_dir, 'xbrl_data'), tensor)
