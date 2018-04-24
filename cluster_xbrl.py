492195ab327eece37410b6f4fbba4adacf7208d7
Mateusz Banaś <banasmat@gmail.com>
2018-04-24 23:22:51 +0200

    Rotating xbrl dataset for heatmap
    Former-commit-id: 170b3ce3a02779e108d0c498a3cdded8e2a0e883
    Former-commit-id: ef01a3c508978940d9afc6e00fa1b06726ee73c4

--- cluster_xbrl.py
+++ cluster_xbrl.py
@@ -47,8 +47,10 @@ cols = [
 dataset_path = os.path.join(os.path.abspath(os.getcwd()), 'output', 'xbrl_dataset', '2017.csv')
 dataset = pd.read_csv(dataset_path, usecols=cols)
 
-cor = dataset.corr() # Calculate the correlation of the above variables
+dataset = dataset.transpose() # 'rotate' 90 degrees
+
+cor = dataset.corr() # Correlation of columns
 
 sns.heatmap(cor, square=True) # Plot the correlation as heat map
-plt.subplots_adjust(bottom=0.1, top=0.9, left=0.07, right=0.87)
+plt.subplots_adjust(bottom=0.2, top=1, left=0.07, right=0.87)
 plt.show()
