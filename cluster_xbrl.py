a78afc82f73e8098ca340064d44ad24ea1e2ec05
Mateusz Banaś <banasmat@gmail.com>
2018-04-24 22:25:10 +0200

    Basic clustering (plot not working)
    Former-commit-id: d0307f8d758c0d372af2336bdfe7009b9172a005
    Former-commit-id: 5720b8921a442907b594f1d03e40c8027aa1b09b

---
+++ cluster_xbrl.py
@@ -0,0 +1,54 @@
+# https://www.kaggle.com/dhanyajothimani/basic-visualization-and-clustering-in-python
+
+import time                   # To time processes
+import warnings               # To suppress warnings
+
+import numpy as np            # Data manipulation
+import pandas as pd           # Dataframe manipulatio
+import matplotlib.pyplot as plt                   # For graphics
+import seaborn as sns
+import plotly.plotly as py #For World Map
+import plotly.graph_objs as go
+# from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+# init_notebook_mode(connected=True)
+
+from sklearn.preprocessing import StandardScaler  # For scaling dataset
+from sklearn.cluster import KMeans, AgglomerativeClustering, AffinityPropagation #For clustering
+from sklearn.mixture import GaussianMixture #For GMM clustering
+
+import os                     # For os related operations
+import sys                    # For data size
+
+cols = [
+ 'AccumulatedDepreciationDepletionAndAmortizationPropertyPlantAndEquipment',
+ 'Assets',
+ 'AssetsCurrent',
+ 'CashAndCashEquivalentsAtCarryingValue',
+ 'CashAndCashEquivalentsPeriodIncreaseDecrease',
+ 'CommonStockParOrStatedValuePerShare',
+ 'CommonStockSharesAuthorized',
+ 'CommonStockSharesIssued',
+ 'CommonStockSharesOutstanding',
+ 'CommonStockValue',
+ 'EntityCommonStockSharesOutstanding',
+ 'EntityPublicFloat',
+ 'IncomeTaxExpenseBenefit',
+ 'Liabilities',
+ 'LiabilitiesAndStockholdersEquity',
+ 'LiabilitiesCurrent',
+ 'NetIncomeLoss',
+ 'OperatingIncomeLoss',
+ 'PropertyPlantAndEquipmentNet',
+ 'RetainedEarningsAccumulatedDeficit',
+ 'StockholdersEquity',
+]
+
+# Importing the dataset
+dataset_path = os.path.join(os.path.abspath(os.getcwd()), 'output', 'xbrl_dataset', '2017.csv')
+dataset = pd.read_csv(dataset_path, usecols=cols)
+
+cor = dataset.corr() # Calculate the correlation of the above variables
+
+sns.heatmap(cor, square=True) # Plot the correlation as heat map
+plt.subplots_adjust(bottom=0.1, top=0.9, left=0.07, right=0.87)
+plt.show()
