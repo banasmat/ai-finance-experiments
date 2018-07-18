9fa8f20dec8020552fecd8592d68541081c94540
Mateusz Banaś <banasmat@gmail.com>
2018-07-18 22:14:37 +0200

    Working on yahoo finance scraper (basic css selectors)
    Former-commit-id: 3c3655ce933e030e114f466e46dbaa36868dffbb
    Former-commit-id: aae5f81cddf6306573494537092daed16d94a345

--- scrapy/spider/spiders/yahoo_finance.py
+++ scrapy/spider/spiders/yahoo_finance.py
@@ -26,8 +26,31 @@ class YahooFinanceSpider(scrapy.Spider):
             self.start_urls.append(self.url_base + symbol + '/cash-flow?p=' + symbol)
 
     def parse(self, response: scrapy.http.response.Response):
-        #TODO
-        # get data
-        # save to file
 
-        data = response.css("[data-test=qsp-financial] tbody").extract()
+        base_css = "[data-test=qsp-financial] tbody "
+
+        datetimes = response.css(base_css + 'tr:first-child td:not(:first-child) span::text').extract()
+
+
+        labels = response.css(base_css + 'tr:not(:first-child) td:first-child span::text').extract()
+        data = response.css(base_css + 'tr:not(:first-child) td:not(:first-child) span::text').extract()
+
+        print(datetimes)
+        print(response.request.url)
+        # quit()
+
+        if '/financials?' in response.request.url:
+            pass
+
+        if '/balance-sheet?' in response.request.url:
+            pass
+
+        if '/cash-flow?' in response.request.url:
+            pass
+
+
+    def make_requests_from_url(self, url):
+        request = super(YahooFinanceSpider, self).make_requests_from_url(url)
+        request.cookies['BX'] = 'dd1vpm1dkv6ve&b=3&s=i2'
+        request.cookies['GUC'] = 'AQABAQFbUOVcIkIhcQTU&s=AQAAAHJpqZ2w&g=W0-b-A'
+        return request
