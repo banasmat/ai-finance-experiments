7960adbcb75da4a8d8dca69b81e76900c1ed8149
Mateusz Banaś <banasmat@gmail.com>
2018-08-20 12:16:44 +0200

    Tried multiple classifiers on yahoo fundamentals dataset
    Former-commit-id: 9cc4724efe7b8b43d3b9903466ae5ebb9739f2b8
    Former-commit-id: adcfcd0ffb63c5e6da67edff6d4924dad4c82ac5

--- yahoo_finance_ml_train.py
+++ yahoo_finance_ml_train.py
@@ -5,6 +5,11 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.svm import SVC
 
 # https://towardsdatascience.com/solving-a-simple-classification-problem-with-python-fruits-lovers-edition-d20ab6b071d2
 
@@ -29,7 +34,7 @@ scaler = MinMaxScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-model = RandomForestClassifier()
+model = SVC()
 
 model = model.fit(X_train, y_train)
 preds = model.predict(X_test)
