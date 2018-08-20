c8e071dbaf1cdacc385fe07b22fbab1e924cae71
Mateusz Banaś <banasmat@gmail.com>
2018-08-20 12:02:48 +0200

    Simple ML script for yahoo finance data
    Former-commit-id: a0ac57b78e9a87db550eb7adb8e6f7a97cf7b8f8
    Former-commit-id: 664a361b7b2b8f5c9cd15b830a429e5102fd9b42

--- yahoo_finance_ml_train.py
+++ yahoo_finance_ml_train.py
@@ -1,10 +1,36 @@
 from app.dataset.XBRLDataSetProvider import XBRLDataSetProvider
 from app.xbrl.XbrlRnn import XbrlRnn
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# https://towardsdatascience.com/solving-a-simple-classification-problem-with-python-fruits-lovers-edition-d20ab6b071d2
 
 x, y = XBRLDataSetProvider.get_dataset_from_yahoo_fundamentals()
 
-# TODO
-# scale wih sklearn scaler
-# flatten data
-# pick algorithm
-# train
+x = np.nan_to_num(x)
+
+# year, cik, tag
+x = x.transpose((2, 0, 1))
+y = y.transpose()
+
+# flattening all years data
+x = np.reshape(x, (x.shape[0] * x.shape[1], x.shape[2]))
+y = np.reshape(y, (y.shape[0] * y.shape[1]))
+
+# using only one year data
+# x = x[0]
+# y = y[0]
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, random_state=0)
+scaler = MinMaxScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+model = RandomForestClassifier()
+
+model = model.fit(X_train, y_train)
+preds = model.predict(X_test)
+print(accuracy_score(y_test, preds))
